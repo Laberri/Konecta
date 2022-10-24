4f6b49f61ce2babcef835e335a9fe72903d9c293
Laberri <116373371+Laberri@users.noreply.github.com>
2022-10-24 14:30:22 -0500

    Commit inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('¡Correcto!') }}
                 </div>
             </div>
         </div>
